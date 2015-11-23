d6f23653a2b1c9230903e986823c1c296c5199c6
Vongola <vongola12324@coder.tw>
2015-11-23 11:54:07 +0800

    finish search algorithm

--- search.py
+++ search.py
@@ -1,9 +1,10 @@
 from sort import *
 
 
-class BinarySearch:
+class LinearSearch:
     def __init__(self, data, target):
-        index = self.search(data, target)
+        newData = list(data)
+        index = self.search(newData, target)
         if not index:
             print("Target: " + str(target) + " Not Found!")
         else:
@@ -13,15 +14,37 @@ class BinarySearch:
         if len(data) < 1:
             return False
         else:
-            newData = list(data)
-            QuickSort(newData)
+            data.append(target)
+            for i in range(0, len(data)-1):
+                if data[i] == target:
+                    data.pop()
+                    return i
+            else:
+                data.pop()
+                return False
+
+
+class BinarySearch:
+    def __init__(self, data, target):
+        newData = list(data)
+        index = self.search(newData, target)
+        if not index:
+            print("Target: " + str(target) + " Not Found!")
+        else:
+            print("Target: " + str(target) + " Found at " + str(index) + " in data!")
+
+    def search(self, data, target):
+        if len(data) < 1:
+            return False
+        else:
+            QuickSort(data)
             low = 0
             high = len(data) - 1
             while low <= high:
                 index = int((low + high) / 2)
-                if newData[index] == target:
+                if data[index] == target:
                     return index
-                elif newData[index] > target:
+                elif data[index] > target:
                     high = index - 1
                 else:
                     low = index + 1
